9a23147d8f98e3d969dfe05f6970e209e393bcdc
hronograf <vlad.boholii@gmail.com>
2020-05-10 20:46:34 +0300

    откат мейна, редиректы, алерт при ошибке
    редиректы при входе и регистрации

--- Registration.js
+++ Registration.js
@@ -1,13 +1,15 @@
 $('#registration').submit(function () {
-    alert("fdffd");
+
     $.post(
-        'http://92.249.117.82:4321/reg',
+        '/reg',
         $("#registration").serialize(),
 
         function (msg) {
-console.log(msg.body);
-            if (typeof msg.redirect == 'string') {
 
+            if (typeof msg.redirect == 'string') {
+                window.location = msg.redirect;
+            }else{
+                alert(msg.message);
             }
         }
     );
