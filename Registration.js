7c99d4d8da69d1f34dac4a2440fe5616d2def7cf
kalinkaaaa14 <61375322+kalinkaaaa14@users.noreply.github.com>
2020-06-11 17:41:39 +0300

    check if passwords are equal in Registration

--- Registration.js
+++ Registration.js
@@ -1,5 +1,5 @@
 $('#registration').submit(function () {
-
+if (document.getElementsByName('password')[0].value === document.getElementsByName('password2')[0].value) {
     $.post(
         '/reg',
 
@@ -8,14 +8,17 @@ $('#registration').submit(function () {
         function (msg) {
 
             if (typeof msg.redirect == 'string') {
-                 window.location = msg.redirect;
+                window.location = msg.redirect;
 
-             }else{
-                 alert(msg.message);
-             }
+            } else {
+                alert(msg.message);
+            }
         }
     );
     return false;
+}else{
+    alert("Incorrect data");
+}
 });
 
 
