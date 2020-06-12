fc409324e1dfe8f1ece85480fc1bbdb342ae415a
hronograf <vlad.boholii@gmail.com>
2020-06-12 19:27:13 +0300

    database fix

--- db_connection.js
+++ db_connection.js
@@ -3,7 +3,8 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    "host": "127.0.0.1",
+    // "host": "127.0.0.1",
+    "host": "92.249.117.82",
     "port": 5432,
     "user": "postgres",
     "password": "hpost5t",
