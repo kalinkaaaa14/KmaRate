e4b47134b92675d6a9174aacf7935b443fbadf52
kalinkaaaa14 <61375322+kalinkaaaa14@users.noreply.github.com>
2021-02-26 00:23:03 +0200

    Update db_connection.js

--- db_connection.js
+++ db_connection.js
@@ -7,22 +7,10 @@ const pool = new Pool({
     "host": "92.249.117.82",
     "port": 5432,
     "user": "postgres",
-    "password": "hpost5t",
+    "password": "",
     "database": "KmaRate",
 });
 
-// pool.query(`
-// CREATE TABLE "session" (
-//   "sid" varchar NOT NULL COLLATE "default",
-//   "sess" json NOT NULL,
-//   "expire" timestamp(6) NOT NULL
-// )
-// WITH (OIDS=FALSE);
-//
-// ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-//
-// CREATE INDEX "IDX_session_expire" ON "session" ("expire");
-// `)
 
 module.exports = pool;
 
